c7d4fc8856aba662374879748ffef11220768348
PoHandsome <louisfat54108@gmail.com>
2021-06-13 17:12:15 +0930

    Fix multiple bugs and reduce the running time

--- translator.py
+++ translator.py
@@ -1,15 +1,24 @@
 from googletrans import Translator
 from gtts import gTTS
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import re
 
 translator = Translator()
 
 def userinput():
     userins = []
     while True:
-        userin = input('Please enter the word(s) or sentence(s) you want to translate (double press enter to stop enter): ')
+        userin = input('Please enter the word(s) you want to translate (double press enter to stop enter): ')
         if userin:
-            userins.append(userin)
+            userindet = userin.split()
+            text = ''
+            for i in userindet:
+                if re.search('[a-zA-Z]', i):
+                    text += i + ' '
+            userins.append(text.strip())
         else:
             break
     return userins
@@ -18,9 +27,12 @@ def get_sentences(userin):
     driver = webdriver.Chrome()
 
     driver.get('https://translate.google.com.au/?hl=zh-TW&tab=rT&sl=en&tl=zh-TW&text='+ userin +'&op=translate')
-    driver.implicitly_wait(5)
-    driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[2]/c-wiz/section/div/div/div[1]/div[2]/div/div[2]/div[1]').click()
-
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[2]/c-wiz/section/div/div/div[1]/div[1]/div/div/h3')))
+    try:
+        driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[2]/c-wiz/section/div/div/div[1]/div[2]/div/div[2]/div[1]').click()
+    except:
+        pass
     ex_sentences = driver.find_elements_by_tag_name('html-blob')
     sens = []
     for i in ex_sentences:
@@ -37,7 +49,10 @@ def trans_to_mp3(userin, sens):
     i = userin
     tts_en1 = gTTS(i + '. ' + i + '. ' + i, slow = True)
     tts_zh = gTTS(dest_text, lang = 'zh-TW')
-    tts_en2 = gTTS('. '.join(sens))
+    if sens == []:
+        tts_en2 = gTTS('No example sentence for this word.')
+    else:
+        tts_en2 = gTTS('. '.join(sens))
     tts_end = gTTS('This is the end of ' + userin)
     with open(userin + '.mp3', 'wb') as f:
         tts_en1.write_to_fp(f)
